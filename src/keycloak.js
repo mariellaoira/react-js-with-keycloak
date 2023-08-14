ad15b3e78cc5773b3e1c00bdf5b0ecda32235c93
Mariella Joyce Oira <117700160+mariellaoira@users.noreply.github.com>
2023-08-15 00:54:15 +0800

    Update keycloak.js

--- src/keycloak.js
+++ src/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
     realm: 'master',
-    clientId: 'elle-projects'
+    clientId: 'elle-projects',
     url: 'http://4.193.163.115:8080'
 };
 
